f1a20bbba2a5a3ec47afd95041869519b91d5761
SantoshKumarRavi <drsanthosh1997@gmail.com>
2022-11-16 21:05:02 +0530

    dele all

--- assign 7/student-api-server/src/model/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const student = new mongoose.Schema({
-    name: String,
-    currentClass:String,
-    division:String
-  });
-
-const Student = mongoose.model('Student', student);
-module.exports=Student
