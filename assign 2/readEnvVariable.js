7931425705458414f828902f0d7dac498f64dc34
SantoshKumarRavi <drsanthosh1997@gmail.com>
2022-11-16 20:28:21 +0530

    dele ass 1 and 2 folder

--- assign 2/readEnvVariable.js
+++
@@ -1,6 +0,0 @@
-//set in the GI
-process.env.USERNAME="some thng" 
-
-
-///one way in terminal (set UserName="santhosh" & node readEnvVariable.js)
-console.log("Hello "+process.env.UserName)
--- assign 2/readFromLine.js
+++
@@ -1,9 +0,0 @@
-var readline = require('readline');
-
-var rl = readline.createInterface(
-     process.stdin, process.stdout);
-
-rl.question('What is your name? ', (name) => {
-console.log('Hello ' + name);
-rl.close()
-});
